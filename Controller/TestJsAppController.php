4021dc5513811aeb70985bf30380b39269fc367d
Joey Trapp <joey@loadsys.com>
2013-04-09 21:12:34 -0500

    If the Auth component is set, allow the index action

--- Controller/TestJsAppController.php
+++ Controller/TestJsAppController.php
@@ -4,5 +4,8 @@ App::uses('AppController', 'Controller');
 
 class TestJsAppController extends AppController {
 	public function beforeFilter() {
+		if (isset($this->Auth)) {
+			$this->Auth->allow('index');
+		}
 	}
 }
