9213a32162342c8e71decafa5222ca325b430ad0
Joey Trapp <joey@loadsys.com>
2013-06-23 20:10:17 -0500

    Calling parent beforeFilter

--- Controller/TestJsAppController.php
+++ Controller/TestJsAppController.php
@@ -4,6 +4,7 @@ App::uses('AppController', 'Controller');
 
 class TestJsAppController extends AppController {
 	public function beforeFilter() {
+		parent::beforeFilter();
 		if (isset($this->Auth)) {
 			$this->Auth->allow('index');
 		}
